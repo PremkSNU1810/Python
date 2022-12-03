50096186f857b71cfaaacb255e283a775245ac25
PremkSNU1810 <119740192+PremkSNU1810@users.noreply.github.com>
2022-12-03 19:52:13 +0530

    Add files via upload

---
+++ python 1.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("prem")
+
+
+# ### prem
+
+# In[2]:
+
+
+####shukla
+
+
+# ####shukla
+
+# #####Prem
+
+# ##cutoo
+
+# In[3]:
+
+
+print(latha)
+
+
+# In[4]:
+
+
+print(\\\\\\\\\\\\\)
+
+
+# In[6]:
+
+
+x=10
+type(x)
+
+
+# In[5]:
+
+
+print(1)
+
+
+# In[7]:
+
+
+z=10.5
+type(z)
+
+
+# In[8]:
+
+
+cousre="Data Science"
+type(cousre)
+
+
+# In[11]:
+
+
+cousr ename="ba"
+cousre name
+
+
+# In[12]:
+
+
+
+
+
+# In[13]:
+
+
+cousre_name="ba"
+cousre_name
+
+
+# In[14]:
+
+
+cousre3name="ba"
+cousre3name
+
+
+# In[15]:
+
+
+3cousrename="ba"
+3cousrename
+
+
+# In[17]:
+
+
+cousre*name="ba"
+cousre*name
+
+
+# In[ ]:
+
+
+
+
