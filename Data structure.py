82af2c780747b96c963c6489ae76b5fe6823a5c8
PremkSNU1810 <119740192+PremkSNU1810@users.noreply.github.com>
2022-12-21 09:55:25 +0530

    Add files via upload

---
+++ Data structure.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+
+# In[3]:
+
+
+sample_list=[1,13.6,"hi",True,12,1]
+sample_list
+
+
+# In[5]:
+
+
+sample_list[2]
+
+
+# In[6]:
+
+
+sample_list[10]
+
+
+# In[8]:
+
+
+sample_list[0]=100
+
+
+# In[4]:
+
+
+sample_list
+
+
+# In[9]:
+
+
+sample_list[-1]
+
+
+# In[10]:
+
+
+sample_list[-2]
+
+
+# In[1]:
+
+
+smaple_tuple=(1,13,"hi",12,1)
+smaple_tuple
+
+
+# In[3]:
+
+
+smaple_tuple[0]
+
+
+# In[4]:
+
+
+smaple_tuple[30]
+
+
+# In[5]:
+
+
+smaple_tuple[3]
+
+
+# In[6]:
+
+
+smaple_tuple[0]=100
+
+
+# In[7]:
+
+
+smaple_set={1,13.6,"hi",12,1,127,25,25,"apple","king","ball"}
+smaple_set
+
+
+# In[8]:
+
+
+
+
+
+# In[9]:
+
+
+smaple_set[0]="hello"
+
+
+# In[10]:
+
+
+smaple_set.add(1000)
+
+
+# In[11]:
+
+
+smaple_set
+
+
+# In[12]:
+
+
+smaple_set.remove("hi")
+
+
+# In[13]:
+
+
+smaple_set
+
+
+# In[14]:
+
+
+sample_dict={1:"apple",2:35.4,3:"Data",2:"every",6:"35.4",7:35.4}
+sample_dict
+
+
+# In[1]:
+
+
+sample_dict[2]
+
+
+# In[15]:
+
+
+sample_dict[1]
+
+
+# In[16]:
+
+
+sample_dict["apple"]
+
+
+# In[17]:
+
+
+sample_dict[7]=100
+
+
+# In[18]:
+
+
+sample_dict
+
+
+# In[19]:
+
+
+sample_dict[10]=business
+
+
+# In[20]:
+
+
+sample_dict
+
+
+# In[ ]:
+
+
+
+
---
+++ numpy.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+arr=np.array([1,2,3])
+arr
+
+
+# In[5]:
+
+
+list_of_lists=[[1,2,3],[4,5,6],[7,8,9]]
+np.array(list_of_lists)
+
+
+# In[7]:
+
+
+np.arange(1,10)
+
+
+# In[8]:
+
+
+np.arange(10)
+
+
+# In[9]:
+
+
+np.arange(100)
+
+
+# In[10]:
+
+
+np.arange(1,46,5)
+
+
+# In[11]:
+
+
+np.zeros(10)
+
+
+# In[12]:
+
+
+np.zeros(10,int)
+
+
+# In[13]:
+
+
+np.zeros(10,dtype=int)
+
+
+# In[14]:
+
+
+np.ones(100)
+
+
+# In[15]:
+
+
+np.ones(100,int)
+
+
+# In[16]:
+
+
+np.ones((2,5))
+
+
+# In[ ]:
+
+
+### create an arrow of zeroes with 2 row and 5 coloumns
+
+
+# In[ ]:
+
+
+import numpy as np
+
+
+# In[17]:
+
+
+np.linspace(0,1,5)  
+
+
+# In[ ]:
+
+
+### Identity matrics
+
+
+# In[18]:
+
+
+np.eye(10)
+
+
+# In[19]:
+
+
+np.random.rand(3,2)
+
+
+# In[20]:
+
+
+np.random.randn(3,2)
+
+
+# In[22]:
+
+
+np.random.randint(34,100)
+
+
+# In[23]:
+
+
+np.random.randint(20,56,100)
+
+
+# In[ ]:
+
+
+
+
